c21ee786ccc4ae641222b046aa39204bde3bee3b
Rafael Figueiredo <rafaellrf09@gmail.com>
2023-11-27 14:53:53 -0300

    feat: create a series controller for api requests

---
+++ app/Http/Controllers/Api/SeriesController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+use App\Repositories\SeriesRepository;
+
+class SeriesController extends Controller
+{
+    public function __construct(
+        private SeriesRepository $seriesRepository
+    ) {
+    }
+    public function index()
+    {
+        return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        return response()->json($this->seriesRepository->add($request), 201);
+    }
+
+    public function show(Series $series)
+    {
+        return $series;
+    }
+
+    public function update(SeriesFormRequest $request, Series $series)
+    {
+        $series->fill($request->all());
+        $series->save();
+        return response()->json($series);
+    }
+
+    public function destroy(int $series)
+    {
+        Series::destroy($series);
+        return response()->noContent();
+    }
+}
