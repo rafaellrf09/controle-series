9993f1aa715fbf3fa552c547f2a9cef70e60d8ee
Rafael Figueiredo <rafaellrf09@gmail.com>
2023-11-28 15:24:15 -0300

    feat: add routesd for login

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\SeriesController;
 use App\Models\Episode;
 use App\Models\Series;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,19 +22,34 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/series', SeriesController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', SeriesController::class);
 
-Route::get("series/{series}/seasons", function(Series $series) {
-    return $series->seasons;
+    Route::get("series/{series}/seasons", function (Series $series) {
+        return $series->seasons;
+    });
+
+    Route::get("series/{series}/episodes", function (Series $series) {
+        return $series->episodes;
+    });
+
+    Route::patch("/episodes/{episode}", function (Episode $episode, Request $request) {
+        $episode->watched = $request->watched;
+        $episode->save();
+
+        return $episode;
+    });
 });
 
-Route::get("series/{series}/episodes", function(Series $series) {
-    return $series->episodes;
-});
 
-Route::patch("/episodes/{episode}", function(Episode $episode, Request $request) {
-    $episode->watched = $request->watched;
-    $episode->save();
+Route::post("login", function (Request $request) {
+    $credentials = $request->only("email", "password");
+    if (!Auth::attempt($credentials)) {
+        return response()->json("Unauthorized", 401);
+    }
 
-    return $episode;
+    $user = Auth::user();
+    $token = $user->createToken("token");
+
+    return response()->json($token->plainTextToken, 200);
 });
