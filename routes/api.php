ca0f2d6328ac16e3e45c4ec2282ec2d2107dace1
Rafael Figueiredo <rafaellrf09@gmail.com>
2023-11-28 13:01:10 -0300

    feat: add new routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\SeriesController;
+use App\Models\Episode;
+use App\Models\Series;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +22,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/series', SeriesController::class);
+
+Route::get("series/{series}/seasons", function(Series $series) {
+    return $series->seasons;
+});
+
+Route::get("series/{series}/episodes", function(Series $series) {
+    return $series->episodes;
+});
+
+Route::patch("/episodes/{episode}", function(Episode $episode, Request $request) {
+    $episode->watched = $request->watched;
+    $episode->save();
+
+    return $episode;
+});
